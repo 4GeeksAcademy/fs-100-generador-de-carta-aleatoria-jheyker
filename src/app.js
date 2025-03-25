b50e686643beae2a759cb89b9045cc65210ca8b3
Jheyker13 <jheyker13@gmail.com>
2025-03-25 15:39:56 +0000

    primer commit del proyecto

--- src/app.js
+++ src/app.js
@@ -4,8 +4,57 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//variables
+const palo =['♦','♥','♠','♣']
+const valor =['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+const cardHeader = document.querySelector('#card-header')
+const cardBody = document.querySelector('#card-body')
+const cardFooter = document.querySelector('#card-footer')
+const cardButton = document.querySelector('#card-button')
+//funciones
+const numRandom = (arr) => {
+  return Math.floor(Math.random ()* arr.length)
+}
+const extraerValor = (arr) => {
+  return arr[numRandom(arr)]
+}
+
+const generarCarta = (arrPalos,arrValor) => {
+  const valorRandom = extraerValor(valor);
+  const paloRandom = extraerValor(palo);
+
+// Limpiar clases anteriores
+   cardHeader.classList.remove('rojo', 'negro');
+   cardBody.classList.remove('rojo', 'negro');
+   cardFooter.classList.remove('rojo', 'negro');
+
+
+// Aplicar las clases de color según el palo
+  if (paloRandom === '♠' || paloRandom === '♣') {
+    cardHeader.classList.add('negro');
+    cardBody.classList.add('negro');
+    cardFooter.classList.add('negro');
+  } else {
+    cardHeader.classList.add('rojo');
+    cardBody.classList.add('rojo');
+    cardFooter.classList.add('rojo');
+  }
+
+  cardHeader.textContent = paloRandom;
+  cardBody.textContent = valorRandom;
+  cardFooter.textContent = paloRandom;
+}
+
+cardButton.addEventListener('click', () => {
+  generarCarta(palo, valor);
+})
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  cardHeader.classList.add('rojo');
+  cardBody.classList.add('rojo');
+  cardFooter.classList.add('rojo')
 };
+
+cardButton.addEventListener('click', () => {
+  generarCarta(palo, valor);
+})
